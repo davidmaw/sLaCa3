efea62ba61a88a85a348cbf9f11dd9c51c9bf711
davidmaw <therealdavidma@gmail.com>
2014-02-13 00:12:35 -0600

    Improved formatting and added additional comments

--- count01sTest.scala
+++ count01sTest.scala
@@ -1,39 +1,45 @@
 package sLaCa3
 
+/**
+ * This sLaCa3 program counts the number of occurences of "01" in the binary string stored at memory location
+ * 0x3100 and writes the count into 0x3101.
+ */
+
 object count01sTest extends sLaCa3{
 	def main(args: Array[String]): Unit = {
-			mem(0x3100) = 0x424F;						//0100001001001111	Expect 4
-			//mem(0x3100) = -1;							//1111111111111111	Expect 0
-			//mem(0x3100) = 1;							//0000000000000001	Expect 1
+			//Different test inputs with their binary representation and expected output.
+			mem(0x3100) = 0x424F;				//0100001001001111	Expect 4
+			//mem(0x3100) = -1;				//1111111111111111	Expect 0
+			//mem(0x3100) = 1;				//0000000000000001	Expect 1
 			//mem(0x3100) = java.lang.Short.MIN_VALUE;	//1000000000000000	Expect 0
-			//mem(0x3100) = 0;							//0000000000000000	Expect 0
+			//mem(0x3100) = 0;				//0000000000000000	Expect 0
 			
 			dot ORIG 0x3000;
 			LDI	R0 "IADDR"; //R0 gets the input value and is shifted left
-			LD R4 "MASK";
+			L	R4 "MASK";
 			ADD	R3 R3 I8;
 			ADD	R3 R3 I8; //R3 will count down to zero to stop looping
 	"AGAIN"	vvv;
 			ADD	R3 R3 I0;
-			BR z "END";
+			BR 	"END";
 			AND	R1 R0 R4; //R1 gets the value of the mask on top of the input
-			BR z "FOUND0";
+			BR 	"FOUND0";
 			ADD	R0 R0 R0;
 			ADD	R3 R3 I_1;
-			BR nzp "AGAIN"	
+			BR nz	"AGAIN"	
 
 	"FOUND0"vvv;
 			ADD	R0 R0 R0;
 			ADD	R3 R3 I_1;
 			AND	R1 R0 R4;
-			BR np "YES";
-			BR nzp "AGAIN";
+			BR n	"YES";
+			BR nz	"AGAIN";
 
 	"YES"	vvv;	
 			ADD	R2 R2 I1;
 			ADD	R0 R0 R0;
 			ADD	R3 R3 I_1;
-			BR nzp "AGAIN";
+			BR nz	"AGAIN";
 
 	"END"	vvv;
 			STI	R2 "OADDR";
